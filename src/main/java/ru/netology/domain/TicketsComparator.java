011379468d1559230b92ec85fcc3dd4a14f8f56a
Alisa68 <lebedeva19682@yandex.ru>
2022-01-24 18:00:48 +0300

    Added comparator

---
+++ src/main/java/ru/netology/domain/TicketsComparator.java
@@ -0,0 +1,10 @@
+package ru.netology.domain;
+
+import java.util.Comparator;
+
+public class TicketsComparator implements Comparator<Ticket> {
+
+    public int compare(Ticket o1, Ticket o2) {
+        return o1.getTravelTime() - o2.getTravelTime();
+    }
+}
